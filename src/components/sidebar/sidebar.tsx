ba019a3b49a1b3b42dda5b3d628c4a98f40306d5
Olivier <olivier.winkler@icloud.com>
2023-05-20 12:57:31 +0200

    feat(app): add rainbowkit config

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Sidebar() {
   return (
@@ -41,11 +41,7 @@ export default function Sidebar() {
         </div>
       </div>
       <div className="p-4">
-        <button type="button" className="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="" viewBox="0 0 16 16">
-            <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-          </svg>
-        </button> <span className="font-bold text-sm ml-2">Logout</span>
+        <ConnectButton />
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,42 @@
-import Layout from '@/layouts/layout'
-import '@/styles/globals.css'
-import { AppProps } from 'next/app'
+import Layout from '@/layouts/layout';
+import '@/styles/globals.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import { AppProps } from 'next/app';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { arbitrum, mainnet, optimism, polygon } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { chains, publicClient } = configureChains(
+    [mainnet, polygon, optimism, arbitrum],
+    [
+      alchemyProvider({ apiKey: process.env.ALCHEMY_ID! }),
+      publicProvider()
+    ]
+  );
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains
+  });
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient
+  })
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   )
 }
