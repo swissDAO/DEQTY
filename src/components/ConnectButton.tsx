7138233bf1506cd826b3ed748aee6b26bae820d7
zaniyar <jaha@zhaw.ch>
2023-05-21 13:55:11 +0200

    Add sismo Magic!

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,5 +1,5 @@
 import { ConnectButton, useConnectModal } from '@rainbow-me/rainbowkit';
-
+import { SismoConnectButton, AuthType, SismoConnectClientConfig, SismoConnectResponse } from "@sismo-core/sismo-connect-react";
 export const ConnectButtonStyled = ({ onClose }: { onClose: () => void }) => {
   const { openConnectModal } = useConnectModal();
 
@@ -115,6 +115,38 @@ export const ConnectButtonStyled = ({ onClose }: { onClose: () => void }) => {
                     )}
                     {chain.name}
                   </button>
+                  <SismoConnectButton
+                    config={{
+                      appId: "0x7749db6b6273afef29cbe138dad02b52"
+                    }}
+                    // request multiple proofs of account ownership 
+                    // (here Vault ownership and Twitter account ownership)
+                    auths={[
+                      { authType: AuthType.VAULT },
+                      { authType: AuthType.TWITTER },
+                    ]}
+                    // request multiple proofs of group membership 
+                    // (here the groups with id 0x42c768bb8ae79e4c5c05d3b51a4ec74a and 0x8b64c959a715c6b10aa8372100071ca7)
+                    claims={[{
+                      groupId: "0xeb8feea83ddaf266a806ee40c1b04fba"//gitcoin passport group
+                    },
+                    {
+                      groupId: "0x091b1dae434d9de5eb51c34874e73351", //custom swissDAO group TBD
+                    }]}
+                    signature={{ message: "Your message" }}
+                    onResponse={async (response: SismoConnectResponse) => {
+                      //Send the response to your server to verify it
+                      //thanks to the @sismo-core/sismo-connect-server package
+                      //
+                    }}
+                    onResponseBytes={async (bytes: string) => {
+                      //Send the response to your contract to verify it
+                      //thanks to the @sismo-core/sismo-connect-solidity package
+                      //now we clal the contract!
+                      //but Oli didn't want to help
+                    }}
+                  />
+
                   <button
                     onClick={openAccountModal}
                     type="button"
