c1ac6e833a6488d5a03927baa87908d60982a63e
Olivier <olivier.winkler@icloud.com>
2023-05-21 07:55:23 +0200

    feat(project): add dialog component

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -9,7 +9,9 @@ import {
   YAxis,
 } from 'recharts';
 import Chart from 'chart.js/auto';
-import { useEffect, useRef } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import Link from 'next/link';
 
 const map = (
   value: number,
@@ -121,9 +123,134 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
+function ContributionDialog({ onClose }: { onClose: () => void }) {
+  return (
+    <Transition.Root show={true} as={Fragment}>
+      <Dialog as="div" className="relative z-10" open={true} onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-gray-900">
+                      Verification Email Sent
+                    </Dialog.Title>
+                    <div className="mt-3">
+                      <p className="text-md text-gray-500">
+                        Check your Inbox for the access link
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <Link href={"/"} onClick={onClose}>
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:text-sm"
+                    >
+                      Try again
+                    </button>
+                  </Link>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root >
+  )
+}
+
+function SettingsDialog({ onClose }: { onClose: () => void }) {
+  return (
+    <Transition.Root show={true} as={Fragment}>
+      <Dialog as="div" className="relative z-10" open={true} onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-gray-900">
+                      Verification Email Sent
+                    </Dialog.Title>
+                    <div className="mt-3">
+                      <p className="text-md text-gray-500">
+                        Check your Inbox for the access link
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <Link href={"/"} onClick={onClose}>
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:text-sm"
+                    >
+                      Try again
+                    </button>
+                  </Link>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root >
+  )
+}
+
 export default function Project() {
   const pieChartRef = useRef<HTMLCanvasElement | null>(null);
   const barChartRef = useRef<HTMLCanvasElement | null>(null);
+
+  const [openContributionDialog, setOpenContributionDialog] = useState(false);
+  const [openSettingsDialog, setOpenSettingsDialog] = useState(false);
 
   useEffect(() => {
     const ctxPie = pieChartRef.current?.getContext("2d");
@@ -188,6 +315,9 @@ export default function Project() {
 
   return (
     <div className="h-screen overflow-auto overflow-x-hidden bg-background p-10">
+      {openContributionDialog && <ContributionDialog onClose={() => setOpenContributionDialog(false)} />}
+      {openSettingsDialog && <SettingsDialog onClose={() => setOpenSettingsDialog(false)} />}
+
       <div className='h-full rounded border border-transparent bg-white p-5'>
         <h1>Project: </h1>
         <div className="flex h-full w-full flex-grow flex-wrap content-start">
